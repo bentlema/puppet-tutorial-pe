2ade891bdb1d0507668886ff26ff3cafe823881c
Mark Bentley <bentlema@yahoo.com>
2016-08-28 10:51:14 -0700

    add vertical format

---
+++ docker/format.sh
@@ -0,0 +1,23 @@
+# .ID          Container ID
+# .Image       Image ID
+# .Command     Quoted command
+# .CreatedAt   Time when the container was created.
+# .RunningFor  Elapsed time since the container was started.
+# .Ports       Exposed ports.
+# .Status      Container status.
+# .Size        Container disk size.
+# .Names       Container names.
+# .Labels      All labels assigned to the container.
+# .Label       Value of a specific label for this container. For example '{{.Label "com.docker.swarm.cpu"}}'
+# .Mounts      Names of the volumes mounted in this container.
+
+FORMAT="
+Container ID: {{.ID}}
+     Names: {{.Names}}
+    Status: {{.Status}}
+     Image: {{.Image}}
+   Command: {{.Command}}
+     Ports: {{.Ports}}
+"
+
+alias dockerps="docker ps --format \"$FORMAT\""
