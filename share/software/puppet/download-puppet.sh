bdc7266c13588b1cb5ba00907f31d19acd7d9973
Mark Bentley <bentlema@yahoo.com>
2016-09-13 10:38:59 -0700

    add more PE download links

--- share/software/puppet/download-puppet.sh
+++ share/software/puppet/download-puppet.sh
@@ -2,15 +2,48 @@
 #
 # Find previous releases here:  https://puppetlabs.com/misc/pe-files/previous-releases
 #
-# This Puppet Enterprise Training uses 3.8.4 on EL7, but if you'd like to try with a newer
-# version, the links for 2015.3.1 are provided.
+# This Puppet Enterprise Training uses 3.8.5 on EL6 or EL7, but if you'd like to try with a newer
+# version, the links for 2015.3.x and 2016.2.x are provided.
+
+#curl -O https://s3.amazonaws.com/pe-builds/released/2016.2.1/puppet-enterprise-2016.2.1-el-7-x86_64.tar.gz
+#curl -O https://s3.amazonaws.com/pe-builds/released/2016.2.1/puppet-enterprise-2016.2.1-el-6-x86_64.tar.gz
+
+#curl -O https://s3.amazonaws.com/pe-builds/released/2016.1.2/puppet-enterprise-2016.1.2-el-7-x86_64.tar.gz
+#curl -O https://s3.amazonaws.com/pe-builds/released/2016.1.2/puppet-enterprise-2016.1.2-el-6-x86_64.tar.gz
+
+#curl -O https://s3.amazonaws.com/pe-builds/released/2016.1.1/puppet-enterprise-2016.1.1-el-7-x86_64.tar.gz
+#curl -O https://s3.amazonaws.com/pe-builds/released/2016.1.1/puppet-enterprise-2016.1.1-el-6-x86_64.tar.gz
+
+#curl -O https://s3.amazonaws.com/pe-builds/released/2016.1.0/puppet-enterprise-2016.1.0-el-7-x86_64.tar.gz
+#curl -O https://s3.amazonaws.com/pe-builds/released/2016.1.0/puppet-enterprise-2016.1.0-el-6-x86_64.tar.gz
+
+#curl -O https://s3.amazonaws.com/pe-builds/released/2015.3.3/puppet-enterprise-2015.3.3-el-7-x86_64.tar.gz
+#curl -O https://s3.amazonaws.com/pe-builds/released/2015.3.3/puppet-enterprise-2015.3.3-el-6-x86_64.tar.gz
+
+#curl -O https://s3.amazonaws.com/pe-builds/released/2015.3.2/puppet-enterprise-2015.3.2-el-7-x86_64.tar.gz
+#curl -O https://s3.amazonaws.com/pe-builds/released/2015.3.2/puppet-enterprise-2015.3.2-el-6-x86_64.tar.gz
+
+#curl -O https://s3.amazonaws.com/pe-builds/released/2015.3.1/puppet-enterprise-2015.3.1-el-7-x86_64.tar.gz
+#curl -O https://s3.amazonaws.com/pe-builds/released/2015.3.1/puppet-enterprise-2015.3.1-el-6-x86_64.tar.gz
+
+#curl -O https://s3.amazonaws.com/pe-builds/released/3.8.7/puppet-enterprise-3.8.7-el-7-x86_64.tar.gz
+#curl -O https://s3.amazonaws.com/pe-builds/released/3.8.7/puppet-enterprise-3.8.7-el-6-x86_64.tar.gz
+
+#curl -O https://s3.amazonaws.com/pe-builds/released/3.8.6/puppet-enterprise-3.8.6-el-7-x86_64.tar.gz
+#curl -O https://s3.amazonaws.com/pe-builds/released/3.8.6/puppet-enterprise-3.8.6-el-6-x86_64.tar.gz
 
 curl -O https://s3.amazonaws.com/pe-builds/released/3.8.5/puppet-enterprise-3.8.5-el-7-x86_64.tar.gz
 curl -O https://s3.amazonaws.com/pe-builds/released/3.8.5/puppet-enterprise-3.8.5-el-6-x86_64.tar.gz
 
 #curl -O https://s3.amazonaws.com/pe-builds/released/3.8.4/puppet-enterprise-3.8.4-el-7-x86_64.tar.gz
+#curl -O https://s3.amazonaws.com/pe-builds/released/3.8.4/puppet-enterprise-3.8.4-el-6-x86_64.tar.gz
+
 #curl -O https://s3.amazonaws.com/pe-builds/released/3.8.3/puppet-enterprise-3.8.3-el-7-x86_64.tar.gz
-#curl -O https://s3.amazonaws.com/pe-builds/released/2015.3.1/puppet-enterprise-2015.3.1-el-7-x86_64.tar.gz
-#curl -O https://s3.amazonaws.com/pe-builds/released/3.8.3/puppet-enterprise-3.8.3-el-6-i386.tar.gz
 #curl -O https://s3.amazonaws.com/pe-builds/released/3.8.3/puppet-enterprise-3.8.3-el-6-x86_64.tar.gz
-#curl -O https://s3.amazonaws.com/pe-builds/released/2015.3.1/puppet-enterprise-2015.3.1-el-6-x86_64.tar.gz
+
+#curl -O https://s3.amazonaws.com/pe-builds/released/3.8.2/puppet-enterprise-3.8.2-el-7-x86_64.tar.gz
+#curl -O https://s3.amazonaws.com/pe-builds/released/3.8.2/puppet-enterprise-3.8.2-el-6-x86_64.tar.gz
+
+#curl -O https://s3.amazonaws.com/pe-builds/released/3.8.1/puppet-enterprise-3.8.1-el-7-x86_64.tar.gz
+#curl -O https://s3.amazonaws.com/pe-builds/released/3.8.1/puppet-enterprise-3.8.1-el-6-x86_64.tar.gz
+
