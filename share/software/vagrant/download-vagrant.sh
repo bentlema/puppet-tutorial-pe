f8c2ed2af52100771b5dd73cd49b7ef6853ee31c
Mark Bentley <bentlema@yahoo.com>
2016-11-11 13:22:13 -0800

    notes on version compatibility

--- share/software/vagrant/download-vagrant.sh
+++ share/software/vagrant/download-vagrant.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 #
+# Not all versions of vagrant work with all version of VirtualBox
+#
+# Versions that I've tested that work well together:
+#
+#     Vagrant 1.8.4 + VirtualBox 4.0.x
 #
 # Note:  1.8.5 has a bug which prevents SSH auth after replacing insecure SSH key.
 #
-#REL='1.8.4'
+REL='1.8.4'
 #REL='1.8.5'  #  <-- Broken release / Do not use
-#REL='1.8.6'
-REL='1.8.7'
+#REL='1.8.6'  #  <-- Not tested / Do not use yet
+#REL='1.8.7'  #  <-- Possible bug on MacOS / Do not use yet
 
 curl -O "https://releases.hashicorp.com/vagrant/${REL}/vagrant_${REL}.dmg"
 curl -O "https://releases.hashicorp.com/vagrant/${REL}/vagrant_${REL}.msi"
